b3a3ca612e06ac12b9e709b1b498748a85fcb984
morovach <morovach@f859b691-235f-41ae-8b56-9e83a4003388>
2014-07-16 14:26:24 +0000

    Good state. Here we have a game launched, with 2 players and 15 minutes per player. Everything is tested and works.
    Now we need to add the menus

--- trunk/pic16_c_template_1.X/system.h
+++ trunk/pic16_c_template_1.X/system.h
@@ -8,38 +8,13 @@
 #define SYS_FREQ        _XTAL_FREQ
 #define FCY             SYS_FREQ/4
 
-#if 1
 //LCD SCREEN
 #define DB7 RB6
 #define DB6 RC6
 #define DB5 RB4
 #define DB4 RC3
-#define DB3 RC2
-#define DB2 RC4
-#define DB1 RC1
-#define DB0 RC5
 #define  EN RC0
 #define  RS RA2
-#define  RW RA5
-#endif
-
-
-#if 0
-//LCD SCREEN
-#define DB7 RB6
-#define DB6 RB4
-#define DB5 RC2
-#define DB4 RC1
-#define DB3 RC0
-#define DB2 RA2
-#define DB1 RA3
-#define DB0 RC6
-#define  EN RC3
-#define  RW RC4
-#define  RS RC5
-#endif
-
-
 
 /******************************************************************************/
 /* System structure definition                                                 */
